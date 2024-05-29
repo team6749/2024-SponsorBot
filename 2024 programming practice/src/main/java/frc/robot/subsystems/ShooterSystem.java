5c15161917b525fa9e30226ed33f384d58dcf3d1
Team 6749 <ERARobotics6749@gmail.com>
2024-05-29 13:39:23 -0500

    Tweaks
    small edit to the stopping mechanism, we probably won't use this branch but might as well optimize.

--- 2024 programming practice/src/main/java/frc/robot/subsystems/ShooterSystem.java
+++ 2024 programming practice/src/main/java/frc/robot/subsystems/ShooterSystem.java
@@ -4,9 +4,13 @@
 
 package frc.robot.subsystems;
 
+import java.util.Currency;
+
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.units.Voltage;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -15,15 +19,26 @@ public class ShooterSystem extends SubsystemBase {
     private TalonFX bottomShooter;
 
     private double Voltage = 5;
+    private double topSpeed = 0;
+    private double currentSpeed = 0;
+    private boolean itemHad = false;
   /** Creates a new ShooterSystem. */
   public ShooterSystem() {
     topShooter = new TalonFX(Constants.electronics.topShooterPort);
     bottomShooter = new TalonFX(Constants.electronics.bottomShooterPort);
   }
+
+    }
   @Override
   public void periodic() {
     topShooter.setVoltage(Voltage);
     bottomShooter.setVoltage(-Voltage);
+    if (Voltage != 0) {
+        currentSpeed = topShooter.getVelocity().getValueAsDouble() + topShooter.getVelocity().getValueAsDouble() * 0.5;
+        if (topSpeed < currentSpeed) {topSpeed = currentSpeed;}
+        if (topSpeed * 0.9 > currentSpeed && itemHad == true) {Voltage = 0;}
+    }
+
     // This method will be called once per scheduler run
   }
   public void shoot(double Voltage) {
