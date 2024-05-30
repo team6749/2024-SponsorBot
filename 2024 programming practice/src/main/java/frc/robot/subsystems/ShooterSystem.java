c9d822da7004d8a97d5a7c6eb5287f4b2c3f2a58
Team 6749 <ERARobotics6749@gmail.com>
2024-05-30 13:08:43 -0500

    Debounce
    Added a debounce to add a time buffer(and update itemHad) to the shooter

--- 2024 programming practice/src/main/java/frc/robot/subsystems/ShooterSystem.java
+++ 2024 programming practice/src/main/java/frc/robot/subsystems/ShooterSystem.java
@@ -8,6 +8,7 @@ import java.util.Currency;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.units.Voltage;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -22,21 +23,28 @@ public class ShooterSystem extends SubsystemBase {
     private double topSpeed = 0;
     private double currentSpeed = 0;
     private boolean itemHad = false;
+    private Debouncer debounce = new Debouncer(0.1);
   /** Creates a new ShooterSystem. */
   public ShooterSystem() {
     topShooter = new TalonFX(Constants.electronics.topShooterPort);
     bottomShooter = new TalonFX(Constants.electronics.bottomShooterPort);
   }
+  //Calculates whether the note has left or not based on the debounce
+  public boolean itemLeft() 
+  {return debounce.calculate(!(topSpeed * 0.9 > currentSpeed && itemHad == true));}
 
-    }
   @Override
   public void periodic() {
     topShooter.setVoltage(Voltage);
     bottomShooter.setVoltage(-Voltage);
+    //detects if the item has left the shooter
     if (Voltage != 0) {
         currentSpeed = topShooter.getVelocity().getValueAsDouble() + topShooter.getVelocity().getValueAsDouble() * 0.5;
         if (topSpeed < currentSpeed) {topSpeed = currentSpeed;}
-        if (topSpeed * 0.9 > currentSpeed && itemHad == true) {Voltage = 0;}
+        if (topSpeed * 0.9 > currentSpeed && itemHad == true) {
+            itemHad = itemLeft();
+            Voltage = 0;
+        }
     }
 
     // This method will be called once per scheduler run
