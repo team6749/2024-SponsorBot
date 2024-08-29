3a5d29690692c458e1195523d3c97d9fba9e6615
Team 6749 <35408444+6749computers@users.noreply.github.com>
2024-08-29 18:53:30 -0500

    Created poi
    Positions of Interest subsystem was made(hopefully completely) so all we need to do is link the limelight up to coral, and create code for it to direct images to a 2d map. then we can hook up the positions of everything set in pois.

---
+++ 2024 programming practice/src/main/java/frc/robot/subsystems/Poi.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class Poi extends Pose2d{
+    //poi = position of interest
+    //define 
+    public Pose2d target;
+    double meterTolerance;
+    double angleTolerance;
+    public String name;
+    
+    public poi
+
+    }
+}
