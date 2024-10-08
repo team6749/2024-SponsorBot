8e989ab04f4fa2321f6a5cef0e22c467489ba056
Team 6749 <35408444+6749computers@users.noreply.github.com>
2024-10-07 19:24:38 -0500

    Finished PID
    up for review, switched SRXs to falcons. implemented state(kinda).

--- 2024 programming practice/src/main/java/frc/robot/subsystems/TankModule.java
+++ 2024 programming practice/src/main/java/frc/robot/subsystems/TankModule.java
@@ -2,13 +2,16 @@ package frc.robot.subsystems;
 
 import java.time.Period;
 
+import com.ctre.phoenix.motorcontrol.MotorCommutation;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import frc.robot.Constants;
@@ -16,18 +19,19 @@ import frc.robot.Constants;
 public class TankModule implements Sendable{
     public String name;
     public  CANcoder encoder;
-    public  WPI_TalonSRX motor;
-    public TalonFX m;
+    public TalonFX motor;
     public Translation2d location;
     public final PIDController veloPID = new PIDController(0, 0, 0);
+    private SwerveModuleState m_targetstate = new SwerveModuleState();
 
     public TankModule(String name, int encoderport, int motorport, Translation2d locationFromCenter) {
         this.name = name;
         encoder = new CANcoder(encoderport);
-        motor = new WPI_TalonSRX(motorport);
+        motor = new TalonFX(motorport);
         location = locationFromCenter;
         veloPID.enableContinuousInput(0, 360);
         this.setModuleNeutralMode(NeutralMode.Brake); 
+        
     }
     public void initSendable(SendableBuilder builder) {
         // TODO Auto-generated method stub
@@ -39,9 +43,15 @@ public class TankModule implements Sendable{
 
         public void periodic() {
             final double driveOutput = veloPID.calculate(getModuleVelocityMs(),
-            state.speedMetersPerSecond);
+            m_targetstate.speedMetersPerSecond);
+            final double driveFeedforward = (m_targetstate.speedMetersPerSecond * 2.8);
+            
+            motor.setVoltage(driveOutput + driveFeedforward);
         }
         public void setModuleNeutralMode(NeutralMode neutralModeValue) {
-        motor.setNeutralMode(neutralModeValue);
+        motor.setNeutralMode(NeutralModeValue.Brake);
+    }
+    public double getModuleVelocityMs() {
+        return motor.getVelocity().getValueAsDouble() / Constants.SwerveConstants.gearRatio * Constants.SwerveConstants.wheelDiameterCm;
     }
 }
